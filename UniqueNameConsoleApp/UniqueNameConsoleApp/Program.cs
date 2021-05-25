44507f1595dd3e194d3c3c416928400cb3e7caf8
Ravikumar <ravikumarachari.s@sonata-software.com>
2021-05-25 10:06:27 +0530

    first commit

---
+++ UniqueNameConsoleApp/UniqueNameConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace UniqueNameConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names1 = new string[] { "Ava", "Emma", "Olivia" };
+            string[] names2 = new string[] { "Olivia", "Sophia", "Emma" };
+            Console.WriteLine(string.Join(", ", MergeNames.UniqueNames(names1, names2))); // should print Ava, Emma, Olivia, Sophia
+            Console.WriteLine("Hello World!");
+        }
+        public class MergeNames
+        {
+            public static string[] UniqueNames(string[] names1, string[] names2)
+            {
+                //am using linq union method - to add two arrays without duplicating and OrderBy for alphetic order of array
+                return ((names1.Union(names2)).OrderBy(x => x).ToArray());
+
+
+            }
+        }
+    }
+}
